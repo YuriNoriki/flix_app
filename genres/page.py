2a949984231134fa80e3cc4843edc67973281e74
yuri noriki <yurinoriki@hotmail.com>
2026-02-20 09:47:44 -0300

    finalizando integração de genres

--- genres/page.py
+++ genres/page.py
@@ -1,25 +1,23 @@
 import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid
-
-
-# Lista de dicionários simulando um banco de dados de gêneros
-genres = [
-  { "id": 1, "name": "Ação" },
-  { "id": 2, "name": "Drama" },
-  { "id": 3, "name": "Comédia" }
-]
+from genres.service import GenresService
 
 
 def show_genres():
-    st.write('Lista de gêneros')
-    # Exibe a lista de gêneros em uma tabela interativa (AgGrid)
-    
-    AgGrid(
-        data = pd.DataFrame(genres), # Converte a lista para DataFrame do Pandas
-        reload_data = True, # Garante a atualização dos dados na tela
-        key = 'genres_grid', # Chave única para o componente
-        )
+    genres_service = GenresService() # Instancia o serviço responsável pelas operações de gênero
+    genres = genres_service.get_genres() # Busca os dados de gêneros através do serviço
+
+    if genres(): # Verifica se a lista de gêneros não está vazia (ajustado de genres() para genres)
+        st.write('Lista de gêneros')
+        genres_df = pd.json_normalize(genres) # Converte a lista de dicionários/JSON para um DataFrame do Pandas
+        AgGrid(  # Exibe a lista de gêneros em uma tabela interativa (AgGrid)
+            data = genres_df, # Converte a lista para DataFrame do Pandas
+            reload_data = True, # Garante a atualização dos dados na tela
+            key = 'genres_grid', # Chave única para o componente
+            )
+    else:
+        st.warning('Nenhum gênero encontrado.')
 
     # Campo de entrada de texto para o nome do novo gênero
     st.title('Cadastrar novo gênero')
@@ -27,5 +25,11 @@ def show_genres():
    
    # Lógica executada ao clicar no botão de cadastro
     if st.button('Cadastrar'):
-        # Exibe mensagem de sucesso (apenas visual neste estágio)
-        st.success(f'Gênero "{name}" cadastrado com sucesso')
+        new_genre = genres_service.create_genre( # Chama o serviço para criar o gênero com o nome inserido
+            name=name,
+        )
+        if new_genre:
+            st.success('Gênero cadastrado com sucesso')
+            st.rerun() # Recarrega a página para atualizar a tabela
+        else:
+            st.error('Erro ao cadastrar o gênero. Verifique os cam')
