b9f45ac1440ba4c34298f13584b848ec2b6e7c3d
yuri noriki <yurinoriki@hotmail.com>
2026-02-20 01:38:21 -0300

    criando service de genres

--- genres/service.py
+++ genres/service.py
@@ -1,38 +1,19 @@
-import streamlit as st
-import requests
-from login.service import logout
+from genres.repository import GenreRepository
 
-class GenreRepository:
-
+class GenresService: # Define a classe de Serviço (contém a lógica de negócio)
+    
     def __init__(self):
-        self.__base_url = "http://127.0.0.1:8000/api/v1/" # URL raiz da API
-        self.__genres_url = f'{self.__base_url}genres/' # URL específica para o recurso de gêneros
-        self.__headers = {
-            'Authorization' : f'Bearer {st.session_state.token}' # Define os cabeçalhos da requisição incluindo o Token de autenticação
-        }
+        # Ao iniciar, cria uma instância do Repositório para poder usar seus métodos
+        self.genre_repository = GenreRepository()
 
     def get_genres(self):
-        response = requests.get( # Faz uma requisição GET enviando o token nos headers para autorização
-            self.__genres_url,
-            headers=self.__headers,
-        )
-        if response.status_code == 200:
-            return response.json()
-        if response.status_code  == 401:
-            logout()
-            return None
-        raise Exception(f' Error ao obter dados da API. Status code {response.status_code}')
+        # Apenas repassa a ordem para o repositório buscar a lista de gêneros
+        return self.genre_repository.get_genres()
     
-    def create_genre(self, genre):
-        response = requests.post(
-            self.__genres_url,
-            headers=self.__headers,
-            data=genre,
+    def create_genre(self, name):
+        # Cria um dicionário com os dados do gênero (prepara o objeto para a API)
+        genre = dict(
+            name=name,
         )
-        if response.status_code == 201:
-            return response.json()
-        if response.status_code  == 401:
-            logout()
-            return None
-        raise Exception(f' Error ao obter dados da API. Status code {response.status_code}')
-    
+        # Envia o dicionário para o repositório realizar a gravação (POST) na API
+        return self.genre_repository.create_genre(genre)
